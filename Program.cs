1e76e5a0ac2305eea053717f9309bc127aa773dc
TLTN <130296037+TLTN@users.noreply.github.com>
2024-01-02 07:50:22 +0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -10,60 +10,65 @@ using System.ComponentModel;
 
 namespace TH6
 {
-    public class Phong
+    public class Person
     {
-        public int songaythue;
-        public double dongia;
+        public string Hoten, Quequan;
+        public int Namsinh;
 
-        public Phong()
+        public Person()
         {
-            songaythue = 0;
-            dongia = 0;
+            Hoten = string.Empty;
+            Quequan= string.Empty;
+            Namsinh = 0;
         }
 
-        public Phong(string loaiphong, int songaythue, double dongia)
+        public Person(string hoten, string quequan, int namsinh)
         {
-            this.songaythue = songaythue;
-            this.dongia = dongia;
+            Hoten = hoten;
+            Quequan = quequan;
+            Namsinh = namsinh;
         }
 
         public virtual void Nhap()
         {
             do
             {
-                Console.Write("Nhập số ngày thuê: "); songaythue = int.Parse(Console.ReadLine());
-            } while (songaythue <= 0);
-        }
-
-        public virtual string LoaiPhong()
-        {
-            return "";
-        } 
-
-        public virtual double TongTien()
-        {
-            return dongia * songaythue;
+                Console.Write("Nhập họ tên: "); Hoten = Console.ReadLine();
+            } while (Hoten == "");
+            do
+            {
+                Console.Write("Nhập quê quán: "); Quequan = Console.ReadLine();
+            } while (Quequan == "");
+            do
+            {
+                Console.Write("Nhập năm sinh: "); Namsinh = int.Parse(Console.ReadLine());
+            } while (Namsinh < 1900);
         }
 
         public virtual void HienThi()
         {
-            Console.Write($"Loại phòng: {LoaiPhong()}\t\t Số ngày thuê: {songaythue}\t\t Trị giá hóa đơn: {TongTien()}\t\t");
+            Console.Write($"Họ tên sinh viên: {Hoten}\t\t Quê quán: {Quequan}\t\t Năm sinh: {Namsinh}\t\t");
         }
     }
 
-    public class PA : Phong
+    public class SinhVien : Person
     {
-        public double dichvu;
+        public int masv;
+        public string lop;
 
-        public PA() : base()
+        public SinhVien() : base()
         {
-            dongia = 80;
-            dichvu = 0;
+            masv = 0;
+            lop = string.Empty;
         }
 
-        public PA(double dichvu) : base()
+        public SinhVien(string ht, string qq, int ns, int masv, string lop) : base(ht, qq, ns)
         {
-            this.dichvu = dichvu;
+            this.masv = masv;
+            this.lop = lop;
+            Hoten = ht;
+            Quequan = qq;
+            Namsinh = ns;
         }
 
         public override void Nhap()
@@ -71,150 +76,61 @@ namespace TH6
             base.Nhap();
             do
             {
-                Console.Write("Nhập số tiền dịch vụ: "); dichvu = double.Parse(Console.ReadLine());
-            } while (dichvu <= 0);    
-        }
-
-        public override string LoaiPhong()
-        {
-            return "A";
-        }
-
-        public override double TongTien()
-        {
-            if (songaythue > 5)
-                return dongia * songaythue * 0.9 + dichvu;
-            return dongia * songaythue + dichvu;
+                Console.Write("Nhập mã sinh viên: "); masv = int.Parse(Console.ReadLine());
+            } while (masv <= 0);
+            do
+            {
+                Console.Write("Nhập lớp: "); lop = Console.ReadLine();
+            } while (lop == "");
         }
 
         public override void HienThi()
         {
             base.HienThi();
-            Console.WriteLine($"Tiền dịch vụ: {dichvu}");
-        }
-    }
-
-    public class PB : Phong
-    {
-        public PB() : base()
-        {
-            dongia = 60;
-        }
-
-        public override void Nhap()
-        {
-            base.Nhap();
-        }
-
-        public override string LoaiPhong()
-        {
-            return "B";
-        }
-
-        public override double TongTien()
-        {
-            if (songaythue > 5)
-                return dongia * songaythue * 0.9;
-            return dongia * songaythue;
-        }
-
-        public override void HienThi()
-        {
-            base.HienThi();
-            Console.WriteLine();
-        }
-    }
-
-    public class PC : Phong
-    {
-        public PC() : base()
-        {
-            dongia = 40;
-        }
-
-        public override void Nhap()
-        {
-            base.Nhap();
-        }
-
-        public override string LoaiPhong()
-        {
-            return "C";
-        }
-
-        public override double TongTien()
-        {
-            return dongia * songaythue;
-        }
-
-        public override void HienThi()
-        {
-            base.HienThi();
-            Console.WriteLine();
+            Console.WriteLine($"Mã sinh viên: {masv}\t Lớp: {lop}");
         }
     }
 
     public class QuanLy
     {
-        private int soluongkh;
-        private Phong[] p;
+        private int soluongsv;
+        private Person[] sv;
 
         public QuanLy()
         {
-            soluongkh = 0;
-            p = new Phong[0];
+            soluongsv = 0;
+            sv = new Person[0];
         }
 
-        public QuanLy(int soluongkh, int n)
+        public QuanLy(int soluongnv, int n)
         {
-            this.soluongkh = soluongkh;
-            p = new Phong[n];
+            this.soluongsv = soluongnv;
+            sv = new Person[n];
         }
 
         public void Nhap()
         {
             do
             {
-                Console.Write("Nhập số lượng thuê phòng: "); soluongkh = int.Parse(Console.ReadLine());
-            } while (soluongkh <= 0);
-            p = new Phong[soluongkh];
+                Console.Write("Nhập số lượng sinh viên: "); soluongsv = int.Parse(Console.ReadLine());
+            } while (soluongsv <= 0);
+            sv = new Person[soluongsv];
 
-            for (int i = 0; i < soluongkh; i++)
-            {
-                int chon;
-                Console.WriteLine($"Phòng thứ {i + 1}");
-                do
-                {
-                    Console.Write("1_Loại A, 2_Loại B, 3_Loại C. Chọn (1-3): ");
-                    chon = int.Parse(Console.ReadLine());
-                } while (chon < 1 || chon > 3);
-                switch (chon)
-                {
-                    case 1:
-                        Phong p1 = new PA();
-                        p1.Nhap();
-                        p[i] = p1;
-                        break;
-                    case 2:
-                        Phong p2 = new PB();
-                        p2.Nhap();
-                        p[i] = p2;
-                        break;
-                    case 3:
-                        Phong p3 = new PC();
-                        p3.Nhap();
-                        p[i] = p3;
-                        break;
-                }
+            for (int i = 0; i < soluongsv; i++)
+            {         
+                Console.WriteLine($"Sinh viên thứ {i + 1}");          
+                Person sv1 = new SinhVien();
+                sv1.Nhap();
+                sv[i] = sv1;
             }
         }
 
         public void HienThi()
         {
-            Console.WriteLine("Danh sách thuê phòng: ");
-            for (int i = 0; i < soluongkh; i++)
+            Console.WriteLine("Danh sách nhân viên: ");
+            for (int i = 0; i < soluongsv; i++)
             {
-                p[i].HienThi();
+                sv[i].HienThi();
             }
         }
     }
